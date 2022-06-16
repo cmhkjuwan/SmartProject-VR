c46bd8daa9467d8dd7e2cbcd5b78eff969afe6d5
Juwan <juwanchen@hk.chinamobile.com>
2022-06-16 15:43:42 +0800

    update ikos

--- js/controlThings.js
+++ js/controlThings.js
@@ -2,4 +2,14 @@ AFRAME.registerComponent('thing-control', {
     schema: {
         isControl: {default: false}
     },
+
+    init: function() {
+        this.el.addEventListener('click', function(evt) {
+            if(isControl) {
+                isControl = false;
+            } else {
+                isControl = true;
+            }
+        });
+    }
 });
